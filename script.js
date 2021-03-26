e8ae2187ff76ac436d6cd68339a3cec9eb996ec9
Manuel Schmalstieg <git.code@ms-studio.net>
2021-03-26 14:41:52 +0100

    Amélioration des cartes
    Style basé sur Inclusive Components de H. Pickering

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-var something = "";
-var anything = "";
+const cards = [...document.querySelectorAll('.card')];
+cards.forEach(card => {
+  card.style.cursor = 'pointer';
+  let down, up, link = card.querySelector('h2 a');
+  // ajouter un délai pour éviter les clics accidentels
+  card.onmousedown = () => down = +new Date();
+  card.onmouseup = () => {
+    up = +new Date();
+    if ((up - down) < 200) {
+      link.click();
+    }
+  }
+});
 
-if (anything == 40) {
-  //this will happen
-}
+// Notes: 
+// ... is a spread / rest operator.
+// cf https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Op%C3%A9rateurs/Syntaxe_d%C3%A9composition
